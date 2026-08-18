bb83f7436e3d4ce671b80fef02d6389e0775a300
Vincent Lorenz <vincent.lorenz@stadt.braunschweig.de>
2026-08-18 16:27:58 +0200

    changed logo

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -4,7 +4,7 @@ import { Dialog, Stack, TextField } from '@fluentui/react'
 import { CopyRegular } from '@fluentui/react-icons'
 
 import { CosmosDBStatus } from '../../api'
-import Contoso from '../../assets/bslwb.svg'
+import Contoso from '../..'
 import { HistoryButton, ShareButton } from '../../components/common/Button'
 import { AppStateContext } from '../../state/AppProvider'
 
